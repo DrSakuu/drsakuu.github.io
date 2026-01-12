ef629f8d56c21d1c75d58c93908d3d7bfdee541b
DrSakuu <sakuukuli@gmail.com>
2026-01-12 08:01:30 +0200

    Update book

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-327ba808.js',
+                path_to_root + 'searchindex-c530c9dd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
